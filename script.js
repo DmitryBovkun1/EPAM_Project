4d03ead8c85bb78ff636dd3ac1f85f29322b78cf
Dmitry_Bovkun <dimon4ikbovkun@gmail.com>
2022-05-10 16:20:37 +0300

    course_part1

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
 const selectEl = document.getElementById('select');
 const registrationImage = document.getElementById('doctor-img');
 
@@ -8,16 +5,3 @@ selectEl.addEventListener('change', function() {
     registrationImage.src = "./Assets/"+this.value+".png"; 
 })
 
-window.onclick = function(event) {
-    if (!event.target.matches('.drop-btn')) {
-
-        const dropdowns = document.getElementsByClassName("dropdown-content");
-        let i;
-        for (i = 0; i < dropdowns.length; i++) {
-            const openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
--- scripts/bd/bd.php
+++
@@ -1,4 +0,0 @@
-<?php
-    $db_connect= new mysqli(Clinic_DBSERVER, Clinic_DBUSER, Clinic_DBPASSWORD, Clinic_DATABASE);
-    $db_connect->set_charset("utf8");
-?>
